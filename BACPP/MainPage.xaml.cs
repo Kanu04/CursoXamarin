88bf312da9a87d7dfed5451a6bd6e0adf8fcdff2
Estudiantes <estudiantes@estacion-200.ucenfotec.local>
2018-09-13 18:46:10 -0500

    Sesion de Xamarin

---
+++ BACPP/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using BACPP.Modelos;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace BACPP
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<object> Items { get; set; } = new ObservableCollection<object> { "1", "2", true, false };
+        public Command AgregarComando { get; set; }
+        public MainPage()
+        {
+            AgregarComando = new Command(async () => await CargarItems());
+            InitializeComponent();
+
+            ButtonAgregar.Clicked += ButtonAgregar_Click;
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await CargarItems();
+        }
+
+        private async void ButtonAgregar_Click (object sender, EventArgs arg)
+        {
+            await CargarItems();
+        }
+
+        private async Task CargarItems()
+        {
+            if (Plugin.Connectivity.CrossConnectivity.Current.IsConnected)
+            {
+                await DisplayAlert("Adverencia", "No hay Internet","Cerrar");
+            }
+
+            IsBusy = true;
+
+            //await Task.Delay(2500);
+
+            //Items.Add($"Elemento #{Items.Count}");
+            Items.Clear();
+            var productos = await CargarProductos();
+
+            foreach (var item in productos)
+            {
+                Items.Add(item);
+            }
+
+            IsBusy = false;
+
+            //await DisplayAlert("titulo", "Hola", "Cerrar");
+        }
+
+        private async Task<Producto[]> CargarProductos()
+        {
+            var cliente = new HttpClient();
+
+            cliente.BaseAddress = new Uri(App.WebServiceUrl);
+            var json = await cliente.GetStringAsync("api/products");
+            Console.WriteLine(json);
+            var resultado = JsonConvert.DeserializeObject<Producto[]>(json);
+
+            return resultado;
+        }
+    }
+}
---
+++ BACPP/Modelos/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BACPP.Modelos
+{
+    public class Producto
+    {
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+
+        public string Quantity { get; set; }
+    }
+}
